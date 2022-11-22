a982cede75d9ebf95dcdc5bd24a7adc5ec9e7037
Rafaela Leal <rafaelalealmartinss@gmail.com>
2022-11-22 09:08:47 -0300

    feat: create main components for thread feature

---
+++ src/components/ThreadList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="thread-list">
+        <h2 class="list-title">Threads</h2>
+        <thread-list-item 
+            v-for="thread in threads" 
+            :key="thread['.key']" 
+            :thread="thread"
+        />
+    </div>
+</template>
+<script>
+import ThreadListItem from './ThreadListItem.vue'
+export default {
+    props: {
+        threads: {
+            type: Array,
+            required: true
+        }
+    },
+    components: {
+        ThreadListItem
+    }
+}
+</script>
---
+++ src/components/ThreadListItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="thread">
+        <div>
+            <p>
+                <a href="#">{{thread.title}}</a>
+            </p>
+            <p class="text-faded text-xsmall">
+                By <a href="#">{{user.name}}</a>, {{thread.publishedAt}}.
+            </p>
+        </div>
+        <div class="activity">
+            <p class="replies-count">
+                {{repliesCount}} replies
+            </p>
+        </div>
+    </div>
+</template>
+<script>
+import sourceData from '@/data.json'
+export default {
+    props: {
+        thread: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        repliesCount() {
+            return Object.keys(this.thread.posts).length - 1
+        },
+        user() {
+            return sourceData.users[this.thread.userId]
+        }
+    }
+}
+</script>
